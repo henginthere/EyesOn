1322dc07a757e78fcba3c00c807b7e93b411cf33
kimjisoo <us13579@naver.com>
2022-10-24 17:44:58 +0900

    [BE]  ADD : DB Table 구축 [S07P31D201-30]

---
+++ BE/src/main/java/com/backend/eyeson/entity/AngelInfoEntity.java
@@ -0,0 +1,47 @@
+package com.backend.eyeson.entity;
+
+import lombok.*;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Builder
+@Entity
+@EntityListeners(AuditingEntityListener.class)
+@Table(name="t_angel_info", schema = "eyeson")
+public class AngelInfoEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "angel_info_seq")
+    private long angelInfoSeq;
+
+    @OneToOne
+    @JoinColumn(name="user_seq")
+    private UserEntity userSeq;
+
+    @Basic
+    @Column(name = "angel_alarm_start")
+    private LocalDateTime angelAlarmStart;
+
+    @Basic
+    @Column(name="angel_alarm_end")
+    private LocalDateTime angelAlarmEnd;
+
+    @Basic
+    @Column(name="angel_alarm_day")
+    private int angelAlarmDay;
+
+    @Basic
+    @Column(name ="angel_comp_cnt")
+    private int angelCompCnt;
+
+    @Basic
+    @Column(name = "angel_help_cnt")
+    private int angelHelpCnt;
+}
