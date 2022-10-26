6521d320a389b2d764da993fe6ca4fc9a94c037f
JeongBJ <ebjdev@gmail.com>
2022-10-26 12:07:08 +0900

    [S07P31D201-62] ADD : ARMLKit | MLKit 추가

---
+++ AOS/Sample/ARMLKit/mlkit/src/main/java/com/d201/mlkit/mlkit/common/Constants.kt
@@ -0,0 +1,31 @@
+package com.d201.mlkit.mlkit.common
+
+const val OBJECT_DETECTION = "Object Detection"
+const val OBJECT_DETECTION_CUSTOM = "Custom Object Detection"
+const val CUSTOM_AUTOML_OBJECT_DETECTION = "Custom AutoML Object Detection (Flower)"
+const val TEXT_RECOGNITION_KOREAN = "Text Recognition Korean (Beta)"
+const val SIZE_SCREEN = "w:screen"
+
+const val TAG = "CameraXLivePreview"
+const val FACE_DETECTION = "Face Detection"
+const val TEXT_RECOGNITION_LATIN = "Text Recognition Latin"
+const val TEXT_RECOGNITION_CHINESE = "Text Recognition Chinese (Beta)"
+const val TEXT_RECOGNITION_DEVANAGARI = "Text Recognition Devanagari (Beta)"
+const val TEXT_RECOGNITION_JAPANESE = "Text Recognition Japanese (Beta)"
+const val BARCODE_SCANNING = "Barcode Scanning"
+const val IMAGE_LABELING = "Image Labeling"
+const val IMAGE_LABELING_CUSTOM = "Custom Image Labeling (Birds)"
+const val CUSTOM_AUTOML_LABELING = "Custom AutoML Image Labeling (Flower)"
+const val POSE_DETECTION = "Pose Detection"
+const val SELFIE_SEGMENTATION = "Selfie Segmentation"
+const val FACE_MESH_DETECTION = "Face Mesh Detection (Beta)";
+
+const val STATE_SELECTED_MODEL = "selected_model"
+
+const val KEY_IMAGE_URI = "com.google.mlkit.vision.demo.KEY_IMAGE_URI"
+const val KEY_IMAGE_MAX_WIDTH = "com.google.mlkit.vision.demo.KEY_IMAGE_MAX_WIDTH"
+const val KEY_IMAGE_MAX_HEIGHT = "com.google.mlkit.vision.demo.KEY_IMAGE_MAX_HEIGHT"
+const val KEY_SELECTED_SIZE = "com.google.mlkit.vision.demo.KEY_SELECTED_SIZE"
+const val REQUEST_IMAGE_CAPTURE = 1001
+const val REQUEST_CHOOSE_IMAGE = 1002
+const val SIZE_ORIGINAL = "w:original" // Original image size
