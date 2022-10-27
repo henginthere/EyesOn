023fb91b6f0ffd10930ca3acfaab4e1036957a38
JeongBJ <ebjdev@gmail.com>
2022-10-27 17:52:55 +0900

    [S07P31D201-62] ADD : ARMLKit | AR + MLKit 임시저장

--- AOS/Sample/ARMLKit/mlkit/src/main/java/com/d201/mlkit/BitmapUtils.java
+++ AOS/Sample/ARMLKit/mlkit/src/main/java/com/d201/mlkit/BitmapUtils.java
@@ -16,7 +16,10 @@
 
 package com.d201.mlkit;
 
+import static androidx.core.math.MathUtils.clamp;
+
 import android.content.ContentResolver;
+import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.ImageFormat;
@@ -28,6 +31,11 @@ import android.media.Image.Plane;
 import android.net.Uri;
 import android.os.Build.VERSION_CODES;
 import android.provider.MediaStore;
+import android.renderscript.Allocation;
+import android.renderscript.Element;
+import android.renderscript.RenderScript;
+import android.renderscript.ScriptIntrinsicYuvToRGB;
+import android.renderscript.Type;
 import android.util.Log;
 
 import androidx.annotation.Nullable;
@@ -284,11 +292,106 @@ public class BitmapUtils {
     }
   }
 
-  public static Bitmap imageToBitmap(Image image){
-    ByteBuffer buffer = image.getPlanes()[0].getBuffer();
-    byte[] bytes = new byte[buffer.remaining()];
-    buffer.get(bytes);
-    Bitmap bitmap = BitmapFactory.decodeByteArray(bytes,0,bytes.length,null);
+  private static ByteBuffer imageToByteBuffer(final Image image)
+  {
+    final Rect crop   = image.getCropRect();
+    final int  width  = crop.width();
+    final int  height = crop.height();
+
+    final Image.Plane[] planes     = image.getPlanes();
+    final byte[]        rowData    = new byte[planes[0].getRowStride()];
+    final int           bufferSize = width * height * ImageFormat.getBitsPerPixel(ImageFormat.YUV_420_888) / 8;
+    final ByteBuffer    output     = ByteBuffer.allocateDirect(bufferSize);
+
+    int channelOffset = 0;
+    int outputStride = 0;
+
+    for (int planeIndex = 0; planeIndex < 3; planeIndex++)
+    {
+      if (planeIndex == 0)
+      {
+        channelOffset = 0;
+        outputStride = 1;
+      }
+      else if (planeIndex == 1)
+      {
+        channelOffset = width * height + 1;
+        outputStride = 2;
+      }
+      else if (planeIndex == 2)
+      {
+        channelOffset = width * height;
+        outputStride = 2;
+      }
+
+      final ByteBuffer buffer      = planes[planeIndex].getBuffer();
+      final int        rowStride   = planes[planeIndex].getRowStride();
+      final int        pixelStride = planes[planeIndex].getPixelStride();
+
+      final int shift         = (planeIndex == 0) ? 0 : 1;
+      final int widthShifted  = width >> shift;
+      final int heightShifted = height >> shift;
+
+      buffer.position(rowStride * (crop.top >> shift) + pixelStride * (crop.left >> shift));
+
+      for (int row = 0; row < heightShifted; row++)
+      {
+        final int length;
+
+        if (pixelStride == 1 && outputStride == 1)
+        {
+          length = widthShifted;
+          buffer.get(output.array(), channelOffset, length);
+          channelOffset += length;
+        }
+        else
+        {
+          length = (widthShifted - 1) * pixelStride + 1;
+          buffer.get(rowData, 0, length);
+
+          for (int col = 0; col < widthShifted; col++)
+          {
+            output.array()[channelOffset] = rowData[col * pixelStride];
+            channelOffset += outputStride;
+          }
+        }
+
+        if (row < heightShifted - 1)
+        {
+          buffer.position(buffer.position() + rowStride - length);
+        }
+      }
+    }
+
+    return output;
+  }
+
+  public static Bitmap imageToBitmap(Image image, Context mContext) {
+
+    // Get the YUV data
+
+    final ByteBuffer yuvBytes = imageToByteBuffer(image);
+
+    // Convert YUV to RGB
+
+    final RenderScript rs = RenderScript.create(mContext);
+
+    final Bitmap        bitmap     = Bitmap.createBitmap(image.getWidth(), image.getHeight(), Bitmap.Config.ARGB_8888);
+    final Allocation allocationRgb = Allocation.createFromBitmap(rs, bitmap);
+
+    final Allocation allocationYuv = Allocation.createSized(rs, Element.U8(rs), yuvBytes.array().length);
+    allocationYuv.copyFrom(yuvBytes.array());
+
+    ScriptIntrinsicYuvToRGB scriptYuvToRgb = ScriptIntrinsicYuvToRGB.create(rs, Element.U8_4(rs));
+    scriptYuvToRgb.setInput(allocationYuv);
+    scriptYuvToRgb.forEach(allocationRgb);
+
+    allocationRgb.copyTo(bitmap);
+    allocationYuv.destroy();
+    allocationRgb.destroy();
+    rs.destroy();
     return bitmap;
+    // Release
+
   }
 }
