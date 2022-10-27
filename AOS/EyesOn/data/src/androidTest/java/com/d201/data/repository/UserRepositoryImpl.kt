8068e333327088d552bab5a4ac5445696b050436
DESKTOP-KVCQHCD\multicampus <aud0725_123@naver.com>
2022-10-27 16:16:18 +0900

    [S07P31D201-80] ADD: UserEntitiy, UserRepositoryImpl

---
+++ AOS/EyesOn/data/src/androidTest/java/com/d201/data/repository/UserRepositoryImpl.kt
@@ -0,0 +1,12 @@
+package com.d201.data.repository
+
+import javax.inject.Inject
+import javax.inject.Singleton
+
+@Singleton
+class UserRepositoryImpl @Inject constructor(
+
+):UserRepository{
+
+
+}
