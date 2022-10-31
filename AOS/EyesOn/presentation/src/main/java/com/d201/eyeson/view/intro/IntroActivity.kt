7fece5e423a72aff52e365f429502df4f463536d
taxfdi6371 <taxfdi6371@gmail.com>
2022-10-31 11:04:43 +0900

    [S07P31D201-124] ADD : 프래그먼트 및 액티비티 화면 추가 설계에 따른 화면 파일 생성

---
+++ AOS/EyesOn/presentation/src/main/java/com/d201/eyeson/view/intro/IntroActivity.kt
@@ -0,0 +1,13 @@
+package com.d201.eyeson.view.intro
+
+import com.d201.eyeson.base.BaseActivity
+import com.d201.eyeson.R
+import com.d201.eyeson.databinding.ActivityIntroBinding
+
+private const val TAG = "IntroActivity"
+class IntroActivity : BaseActivity<ActivityIntroBinding>(R.layout.activity_intro) {
+    override fun init() {
+        TODO("Not yet implemented")
+    }
+
+}
---
+++ AOS/EyesOn/presentation/src/main/java/com/d201/eyeson/view/login/join/SelectGenderFragment.kt
@@ -0,0 +1,14 @@
+package com.d201.eyeson.view.login.join
+
+import com.d201.eyeson.R
+import com.d201.eyeson.base.BaseFragment
+import com.d201.eyeson.databinding.FragmentSelectGenderBinding
+
+private const val TAG = "SelectGenderFragment"
+class SelectGenderFragment : BaseFragment<FragmentSelectGenderBinding>(R.layout.fragment_select_gender) {
+
+    override fun init() {
+        TODO("Not yet implemented")
+    }
+
+}
