2930f5aec0d6916e4f0e7b8013ff36bf59fb928b
JeongBJ <ebjdev@gmail.com>
2022-10-31 13:55:07 +0900

    [S07P31D201-62] ADD : 장애물 인식 |  추가 완료

--- AOS/EyesOn/presentation/src/main/java/com/d201/eyeson/view/blind/scanobstacle/ScanObstacleActivity.kt
+++ AOS/EyesOn/presentation/src/main/java/com/d201/eyeson/view/blind/scanobstacle/ScanObstacleActivity.kt
@@ -2,13 +2,25 @@ package com.d201.eyeson.view.blind.scanobstacle
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.d201.depth.depth.common.FullScreenHelper
 import com.d201.eyeson.R
 import com.d201.eyeson.base.BaseActivity
 import com.d201.eyeson.databinding.ActivityScanObstacleBinding
 
+private const val TAG = "ScanObstacleActivity"
 class ScanObstacleActivity : BaseActivity<ActivityScanObstacleBinding>(R.layout.activity_scan_obstacle){
     override fun init() {
-        TODO("Not yet implemented")
+        initView()
     }
 
+    private fun initView(){
+        binding.apply {
+            supportFragmentManager.beginTransaction().replace(R.id.frame_scan_obstacle, ScanObstacleFragment()).commit()
+        }
+    }
+
+    override fun onWindowFocusChanged(hasFocus: Boolean) {
+        super.onWindowFocusChanged(hasFocus)
+        FullScreenHelper.setFullScreenOnWindowFocusChanged(this, hasFocus)
+    }
 }
