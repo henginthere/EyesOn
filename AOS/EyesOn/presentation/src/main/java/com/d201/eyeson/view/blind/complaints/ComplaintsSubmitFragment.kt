e3c7864e0dd5b0fbc7c622edc2261c6c253ff274
taxfdi6371 <taxfdi6371@gmail.com>
2022-11-01 10:38:00 +0900

    [S07P31D201-124] UPDATE : 민원 등록 화면 추가
    빠진 화면 추가

---
+++ AOS/EyesOn/presentation/src/main/java/com/d201/eyeson/view/blind/complaints/ComplaintsSubmitFragment.kt
@@ -0,0 +1,14 @@
+package com.d201.eyeson.view.blind.complaints
+
+import com.d201.eyeson.R
+import com.d201.eyeson.base.BaseFragment
+import com.d201.eyeson.databinding.FragmentComplaintsSubmitBinding
+
+private const val TAG = "ComplaintsSubmitFragment"
+class ComplaintsSubmitFragment : BaseFragment<FragmentComplaintsSubmitBinding>(R.layout.fragment_complaints_submit) {
+
+    override fun init() {
+        TODO("Not yet implemented")
+    }
+
+}
