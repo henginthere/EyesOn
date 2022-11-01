9352ad6afc9491419c8f8d1be30f85fdfd92b322
JeongBJ <ebjdev@gmail.com>
2022-11-01 15:48:55 +0900

    [S07P31D201-62] FIX : Login | AngelInfo DTO 수정

--- AOS/EyesOn/domain/src/main/java/com/d201/domain/model/AngelInfo.kt
+++ AOS/EyesOn/domain/src/main/java/com/d201/domain/model/AngelInfo.kt
@@ -4,11 +4,11 @@ import com.google.gson.annotations.SerializedName
 import java.time.LocalDateTime
 
 data class AngelInfo(
-    val alarmStart: LocalDateTime?,
-    val alarmEnd: LocalDateTime?,
-    val alarmDay: Int?,
-    val compCnt: Int?,
-    val helpCnt: Int?,
+    val alarmStart: Int,
+    val alarmEnd: Int,
+    val alarmDay: Int,
+    val compCnt: Int,
+    val helpCnt: Int,
     val gender: String,
     val active: Boolean
 )
