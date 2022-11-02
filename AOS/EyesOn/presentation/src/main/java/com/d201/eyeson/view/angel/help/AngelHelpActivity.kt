8a4fe36b8a97d46c54357e2773bfda2c52b3d5f5
taxfdi6371 <taxfdi6371@gmail.com>
2022-11-02 17:56:33 +0900

    [S07P31D201-146] ADD : Webrtc 화면 추가 및 구현 중
    오류 수정 필요...

---
+++ AOS/EyesOn/presentation/src/main/java/com/d201/eyeson/view/angel/help/AngelHelpActivity.kt
@@ -0,0 +1,205 @@
+package com.d201.eyeson.view.angel.help
+
+import android.Manifest
+import android.content.pm.PackageManager
+import android.media.AudioManager
+import android.util.Base64
+import android.util.Log
+import androidx.core.content.ContextCompat
+import com.d201.eyeson.R
+import com.d201.eyeson.base.BaseActivity
+import com.d201.eyeson.databinding.ActivityAngelHelpBinding
+import com.d201.eyeson.util.OPENVIDU_SECRET
+import com.d201.eyeson.util.OPENVIDU_URL
+import com.d201.webrtc.openvidu.LocalParticipant
+import com.d201.webrtc.openvidu.Session
+import com.d201.webrtc.utils.CustomHttpClient
+import com.d201.webrtc.websocket.CustomWebSocket
+import com.gun0912.tedpermission.PermissionListener
+import com.gun0912.tedpermission.normal.TedPermission
+import dagger.hilt.android.AndroidEntryPoint
+import okhttp3.Call
+import okhttp3.Callback
+import okhttp3.MediaType.Companion.toMediaTypeOrNull
+import okhttp3.RequestBody
+import okhttp3.Response
+import org.json.JSONException
+import org.json.JSONObject
+import org.webrtc.EglBase
+import java.io.IOException
+
+private const val TAG = "AngelHelpActivity"
+@AndroidEntryPoint
+class AngelHelpActivity : BaseActivity<ActivityAngelHelpBinding>(R.layout.activity_angel_help) {
+
+    private lateinit var session: Session
+    private lateinit var httpClient: CustomHttpClient
+    private var toggle = true
+    private lateinit var customerId: String
+    private lateinit var customerName: String
+    private lateinit var audioManager: AudioManager
+    private var isSpeaker = false
+
+    override fun init() {
+        checkPermission()
+    }
+
+    override fun onResume() {
+        super.onResume()
+
+        if (allPermissionsGranted()) {
+            initView()
+            httpClient = CustomHttpClient(
+                OPENVIDU_URL, "Basic " + Base64.encodeToString(
+                    "OPENVIDUAPP:$OPENVIDU_SECRET".toByteArray(), Base64.DEFAULT
+                ).trim()
+            )
+
+            val sessionId = "customerId" + "-session"
+            getToken(sessionId)
+        }
+    }
+
+    fun initView() {
+        binding.apply {
+            val rootEgleBase = EglBase.create()
+            svrLocal.init(rootEgleBase.eglBaseContext, null)
+            svrLocal.setMirror(true)
+            svrLocal.setEnableHardwareScaler(true)
+            svrLocal.setZOrderMediaOverlay(true)
+        }
+    }
+
+    private fun getToken(sessionId: String) {
+        try {
+            // Session Request
+            val sessionBody: RequestBody = RequestBody.create(
+                "application/json; charset=utf-8".toMediaTypeOrNull(),
+                "{\"customSessionId\": \"$sessionId\"}"
+            )
+            httpClient.httpCall(
+                "/openvidu/api/sessions",
+                "POST",
+                "application/json",
+                sessionBody,
+                object : Callback {
+                    @Throws(IOException::class)
+                    override fun onResponse(call: Call, response: Response) {
+                        Log.d(TAG, "responseString: " + response.body!!.string())
+
+                        // Token Request
+                        val tokenBody: RequestBody =
+                            RequestBody.create(
+                                "application/json; charset=utf-8".toMediaTypeOrNull(),
+                                "{}"
+                            )
+                        httpClient.httpCall(
+                            "/openvidu/api/sessions/$sessionId/connection",
+                            "POST",
+                            "application/json",
+                            tokenBody,
+                            object : Callback {
+                                override fun onResponse(call: Call, response: Response) {
+                                    var responseString: String? = null
+                                    try {
+                                        responseString = response.body!!.string()
+                                    } catch (e: IOException) {
+                                        Log.e(TAG, "Error getting body", e)
+                                    }
+                                    Log.d(TAG, "responseString2: $responseString")
+                                    var tokenJsonObject: JSONObject? = null
+                                    var token: String? = null
+                                    try {
+                                        tokenJsonObject = JSONObject(responseString)
+                                        token = tokenJsonObject.getString("token")
+                                    } catch (e: JSONException) {
+                                        e.printStackTrace()
+                                    }
+                                    getTokenSuccess(token!!, sessionId)
+                                }
+
+                                override fun onFailure(call: Call, e: IOException) {
+                                    Log.e(TAG, "Error POST /api/tokens", e)
+                                    viewToDisconnectedState()
+                                }
+                            })
+                    }
+
+                    override fun onFailure(call: Call, e: IOException) {
+                        Log.e(TAG, "Error POST /api/sessions", e)
+                        viewToDisconnectedState()
+                    }
+                })
+        } catch (e: IOException) {
+            Log.e(TAG, "Error getting token", e)
+            e.printStackTrace()
+            viewToDisconnectedState()
+        }
+    }
+
+    private fun getTokenSuccess(token: String, sessionId: String) {
+        // Initialize our session
+        session = Session(sessionId, token, this, binding.rlContainer)
+
+        // Initialize our local participant and start local camera
+        val participantName: String = "customerName"
+        val localParticipant =
+            LocalParticipant(
+                participantName,
+                session,
+                this.applicationContext,
+                binding.svrLocal
+            )
+        localParticipant.startCamera()
+
+        // Initialize and connect the websocket to OpenVidu Server
+        startWebSocket()
+    }
+
+    fun viewToDisconnectedState() {
+        runOnUiThread {
+            binding.apply {
+                svrLocal.clearImage()
+                svrLocal.release()
+            }
+        }
+    }
+
+    private fun startWebSocket() {
+        val webSocket = CustomWebSocket(session, OPENVIDU_URL, this)
+        webSocket.execute()
+        session.setWebSocket(webSocket)
+    }
+
+    private fun allPermissionsGranted() = mutableListOf(
+        Manifest.permission.CAMERA,
+        Manifest.permission.RECORD_AUDIO,
+        Manifest.permission.MODIFY_AUDIO_SETTINGS
+    ).toTypedArray().all {
+        ContextCompat.checkSelfPermission(
+            baseContext, it
+        ) == PackageManager.PERMISSION_GRANTED
+    }
+
+    private fun checkPermission() {
+        val permissionListener = object : PermissionListener {
+            override fun onPermissionGranted() {
+            }
+
+            override fun onPermissionDenied(deniedPermissions: List<String>) {
+                showToast("권한을 허용해야 이용이 가능합니다.")
+                finish()
+            }
+
+        }
+        TedPermission.create()
+            .setPermissionListener(permissionListener)
+            .setDeniedMessage("권한을 허용해주세요. [설정] > [앱 및 알림] > [고급] > [앱 권한]")
+            .setPermissions(
+                Manifest.permission.CAMERA,
+                Manifest.permission.RECORD_AUDIO,
+                Manifest.permission.MODIFY_AUDIO_SETTINGS
+            )
+            .check()
+    }
+}
