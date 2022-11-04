225f0b7bfe88e3bf3d348a792c4be8bcd4685953
taxfdi6371 <taxfdi6371@gmail.com>
2022-11-04 15:30:11 +0900

    [S07P31D201-146] ADD : Help API 추가

---
+++ AOS/EyesOn/data/src/main/java/com/d201/data/datasource/HelpRemoteDataSource.kt
@@ -0,0 +1,26 @@
+package com.d201.data.datasource
+
+import com.d201.data.api.HelpApi
+import com.d201.domain.base.BaseResponse
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+import javax.inject.Inject
+import javax.inject.Singleton
+
+@Singleton
+class HelpRemoteDataSource @Inject constructor(
+    private val helpApi: HelpApi
+){
+
+    fun requestHelp(gender: String) : Flow<BaseResponse<Int>> = flow {
+        emit(helpApi.requestHelp(gender))
+    }
+
+    fun responseHelp() : Flow<BaseResponse<Int>> = flow {
+        emit(helpApi.responseHelp())
+    }
+
+    fun disconnectHelp() : Flow<BaseResponse<Void>> = flow {
+        emit(helpApi.disconnectHelp())
+    }
+}
