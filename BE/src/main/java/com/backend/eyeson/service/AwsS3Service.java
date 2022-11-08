eca40909e54337346cd8f7f13d4d1ce620558f54
DESKTOP-KVCQHCD\multicampus <sa01070@naver.com>
2022-11-08 10:16:38 +0900

    [[S07P31D201-115] FIX: 민원 API 수정

--- BE/src/main/java/com/backend/eyeson/service/AwsS3Service.java
+++ BE/src/main/java/com/backend/eyeson/service/AwsS3Service.java
@@ -70,12 +70,12 @@ public class AwsS3Service {
     private static final String TIME_SEPARATOR = "_";
 
     public static String buildFileName(String category, String originalFileName) {
-        int fileExtensionIndex = originalFileName.lastIndexOf(FILE_EXTENSION_SEPARATOR);
-        String fileExtension = originalFileName.substring(fileExtensionIndex);
-        String fileName = originalFileName.substring(0, fileExtensionIndex);
-        String now = String.valueOf(System.currentTimeMillis());
+//        int fileExtensionIndex = originalFileName.lastIndexOf(FILE_EXTENSION_SEPARATOR);
+//        String fileExtension = originalFileName.substring(fileExtensionIndex);
+//        String fileName = originalFileName.substring(0, fileExtensionIndex);
+//        String now = String.valueOf(System.currentTimeMillis());
 
-        return fileName + TIME_SEPARATOR + now + fileExtension;
+        return originalFileName;
     }
 
 
