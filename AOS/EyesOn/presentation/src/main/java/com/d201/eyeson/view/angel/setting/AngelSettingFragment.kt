a05441bc784f2bbce403dfbfb9e29ca24e8322b7
taxfdi6371 <taxfdi6371@gmail.com>
2022-11-08 17:58:22 +0900

    [S07P31D201-194] UPDATE : 엔젤 알림 설정 기능 작성 중

--- AOS/EyesOn/presentation/src/main/java/com/d201/eyeson/view/angel/setting/AngelSettingFragment.kt
+++ AOS/EyesOn/presentation/src/main/java/com/d201/eyeson/view/angel/setting/AngelSettingFragment.kt
@@ -1,18 +1,33 @@
 package com.d201.eyeson.view.angel.setting
 
+import android.content.Intent
+import android.widget.CompoundButton
+import android.widget.ToggleButton
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.findNavController
 import com.d201.eyeson.R
 import com.d201.eyeson.base.BaseFragment
 import com.d201.eyeson.databinding.FragmentAngelSettingBinding
+import com.d201.eyeson.view.login.LoginActivity
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.launch
+import java.io.File
 
 private const val TAG = "AngelSettingFragment"
+
 @AndroidEntryPoint
-class AngelSettingFragment : BaseFragment<FragmentAngelSettingBinding>(R.layout.fragment_angel_setting) {
+class AngelSettingFragment :
+    BaseFragment<FragmentAngelSettingBinding>(R.layout.fragment_angel_setting) {
+
+    private val angelSettingViewModel : AngelSettingViewModel by viewModels()
+
+    private var toggleDays = listOf(binding.toggleSun, binding.toggleMon, binding.toggleTue, binding.toggleWed, binding.toggleThu, binding.toggleFri, binding.toggleSat)
 
     override fun init() {
         initView()
-        initViewModel()
+        initViewModelCallback()
     }
 
     private fun initView() {
@@ -20,20 +35,62 @@ class AngelSettingFragment : BaseFragment<FragmentAngelSettingBinding>(R.layout.
             btnBack.setOnClickListener {
                 findNavController().popBackStack()
             }
-            btnLogout.setOnClickListener {
 
+            switchAlarm.setOnCheckedChangeListener { compoundButton, b ->
+
+            }
+            btnLogout.setOnClickListener {
+                clearApplicationData()
+                startActivity(Intent(requireActivity(), LoginActivity::class.java))
+                requireActivity().finish()
             }
             btnResign.setOnClickListener {
-
-            }
-            switchAlarm.setOnCheckedChangeListener { compoundButton, b ->
 
             }
         }
     }
 
-    private fun initViewModel() {
-        TODO("Not yet implemented")
+    private fun initViewModelCallback() {
+        lifecycleScope.launch {
+            angelSettingViewModel.angelInfo.collectLatest {
+                val alarmDay = it!!.alarmDay
+
+                for (i in 0 .. 6){
+                    if(0 != (alarmDay and (64 shr i))){
+                        toggleDays[i].isChecked = true
+                    }
+                }
+            }
+        }
     }
+
+    // 데이터 삭제
+    fun clearApplicationData() {
+        val cache: File = requireActivity().cacheDir
+        val appDir = File(cache.parent)
+        if (appDir.exists()) {
+            val children = appDir.list()
+            for (s in children!!) {
+                if (s != "lib" && s != "shared_prefs") {
+                    deleteDir(File(appDir, s))
+                }
+            }
+        }
+    }
+
+    fun deleteDir(dir: File?): Boolean {
+        if (dir != null && dir.isDirectory) {
+            val children: Array<String> = dir.list()
+            for (i in children.indices) {
+                val success = deleteDir(File(dir, children[i]))
+                if (!success) {
+                    return false
+                }
+            }
+        }
+        // The directory is now empty or this is a file so delete it
+        return dir!!.delete()
+    }
+
 
 }
--- AOS/EyesOn/presentation/src/main/java/com/d201/eyeson/view/angel/setting/AngelSettingViewModel.kt
+++ AOS/EyesOn/presentation/src/main/java/com/d201/eyeson/view/angel/setting/AngelSettingViewModel.kt
@@ -4,6 +4,7 @@ import android.util.Log
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.d201.domain.model.AngelInfo
+import com.d201.domain.usecase.user.GetAngelInfoUseCase
 import com.d201.domain.usecase.user.PutAngelInfoUseCase
 import com.d201.domain.utils.ResultType
 import dagger.hilt.android.lifecycle.HiltViewModel
@@ -16,10 +17,25 @@ import javax.inject.Inject
 
 private const val TAG = "AngelSettingViewModel"
 @HiltViewModel
-class AngelSettingViewModel @Inject constructor(private val putAngelInfoUseCase: PutAngelInfoUseCase): ViewModel() {
+class AngelSettingViewModel @Inject constructor(
+    private val angelInfoUseCase: GetAngelInfoUseCase,
+    private val putAngelInfoUseCase: PutAngelInfoUseCase
+    ): ViewModel() {
 
     private val _angelInfo: MutableStateFlow<AngelInfo?> = MutableStateFlow(null)
     val angelInfo get() = _angelInfo.asStateFlow()
+
+    fun getAngelInfo(){
+        viewModelScope.launch(Dispatchers.IO){
+            angelInfoUseCase.excute().collectLatest {
+                if(it is ResultType.Success && it.data.status == 200){
+                    _angelInfo.value = it.data.data
+                }else{
+                    Log.d(TAG, "getAngelInfo: ${it}")
+                }
+            }
+        }
+    }
 
     fun putAngelInfo(alarmStart: Int, alarmEnd: Int, alarmDay: Int, active: Boolean){
         viewModelScope.launch(Dispatchers.IO){
