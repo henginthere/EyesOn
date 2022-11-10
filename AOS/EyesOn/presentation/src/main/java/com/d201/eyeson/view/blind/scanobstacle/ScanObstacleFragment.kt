425d391afe53c1e992078f21a3c6a52c6892cd45
JeongBJ <ebjdev@gmail.com>
2022-11-10 09:06:12 +0900

    [S07P31D201-138]  UPDATE : 장애물 인식 | 인식률 변경, 표시

--- AOS/EyesOn/presentation/src/main/java/com/d201/eyeson/view/blind/scanobstacle/ScanObstacleFragment.kt
+++ AOS/EyesOn/presentation/src/main/java/com/d201/eyeson/view/blind/scanobstacle/ScanObstacleFragment.kt
@@ -458,7 +458,7 @@ class ScanObstacleFragment : BaseFragment<FragmentScanObstacleBinding>(R.layout.
         // Step 2: Initialize the detector object
         val options = ObjectDetector.ObjectDetectorOptions.builder()
             .setMaxResults(1)
-            .setScoreThreshold(0.7f)
+            .setScoreThreshold(0.4f)
             .build()
         val detector = ObjectDetector.createFromFileAndOptions(
             requireContext(),
@@ -473,8 +473,8 @@ class ScanObstacleFragment : BaseFragment<FragmentScanObstacleBinding>(R.layout.
         return results.map {
             // Get the top-1 category and craft the display text
             val category = it.categories.first()
-//            val text = "${category.label}, ${category.score.times(100).toInt()}%"//100
-            val text = "${category.label}"//100
+            val text = "${category.label}, ${category.score.times(100).toInt()}%"//100
+//            val text = "${category.label}"//100
 
             // 탐지 결과를 표시할 데이터 객체 생성
             DetectionResult(it.boundingBox, text)
