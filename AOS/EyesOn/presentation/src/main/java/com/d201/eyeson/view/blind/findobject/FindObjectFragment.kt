a3039171c44fceefe9b6ece74502c303440d07b7
JeongBJ <ebjdev@gmail.com>
2022-11-10 10:32:24 +0900

    [S07P31D201-143] ADD : 물건 찾기 | 레이아웃 추가

--- AOS/EyesOn/presentation/src/main/java/com/d201/eyeson/view/blind/findobject/FindObjectFragment.kt
+++ AOS/EyesOn/presentation/src/main/java/com/d201/eyeson/view/blind/findobject/FindObjectFragment.kt
@@ -1,9 +1,16 @@
 package com.d201.eyeson.view.blind.findobject
 
+import androidx.lifecycle.lifecycleScope
 import com.d201.eyeson.R
 import com.d201.eyeson.base.BaseFragment
 import com.d201.eyeson.databinding.FragmentFindObjectBinding
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlin.concurrent.timer
+import kotlin.concurrent.timerTask
 
 private const val TAG = "FindObjectFragment"
 @AndroidEntryPoint
