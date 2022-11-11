8b1a949a0eb8062fa33032a6f07f54284db8c8ed
DESKTOP-KVCQHCD\multicampus <aud0725_123@naver.com>
2022-11-11 14:36:39 +0900

    [S07P31D201-214] UPDATE : 로그아웃

--- AOS/EyesOn/presentation/src/main/java/com/d201/eyeson/view/blind/setting/BlindSettingFragment.kt
+++ AOS/EyesOn/presentation/src/main/java/com/d201/eyeson/view/blind/setting/BlindSettingFragment.kt
@@ -1,10 +1,13 @@
 package com.d201.eyeson.view.blind.setting
 
+import android.app.AlertDialog
+import android.content.Intent
 import android.content.SharedPreferences
 import com.d201.eyeson.R
 import com.d201.eyeson.base.BaseFragment
 import com.d201.eyeson.databinding.FragmentBlindSettingBinding
 import com.d201.eyeson.util.JWT
+import com.d201.eyeson.view.login.LoginActivity
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
@@ -26,7 +29,7 @@ class BlindSettingFragment : BaseFragment<FragmentBlindSettingBinding>(R.layout.
             btnReplayGuide.setOnClickListener {  }
 
             btnLogout.setOnClickListener {
-                sharedPref.edit().remove(JWT)
+                showDialog()
             }
 
             btnSignOut.setOnClickListener {
@@ -34,4 +37,21 @@ class BlindSettingFragment : BaseFragment<FragmentBlindSettingBinding>(R.layout.
             }
         }
     }
+    private fun showDialog(){
+        val builder = AlertDialog.Builder(requireContext())
+        builder
+            .setTitle("로그아웃 할까요?")
+            .setIcon(R.drawable.ic_launcher_foreground)
+            .setPositiveButton("예") { dialog , which ->
+                sharedPref.edit().remove(JWT)
+                startActivity(Intent(requireContext(), LoginActivity::class.java))
+                requireActivity().finish()
+            }
+            .setNegativeButton("취소"){ dialog, which ->
+                dialog.dismiss()
+            }
+            .create()
+            .show()
+    }
+    
 }
