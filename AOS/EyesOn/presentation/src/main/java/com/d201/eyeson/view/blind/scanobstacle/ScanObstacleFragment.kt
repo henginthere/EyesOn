8dc04823d1ef35176cf48777e9fed0be3999cf74
JeongBJ <ebjdev@gmail.com>
2022-11-15 02:44:52 +0900

    [S07P31D201-144] ADD : 장애물 인식 | 방향 안내

--- AOS/EyesOn/presentation/src/main/java/com/d201/eyeson/view/blind/scanobstacle/ScanObstacleFragment.kt
+++ AOS/EyesOn/presentation/src/main/java/com/d201/eyeson/view/blind/scanobstacle/ScanObstacleFragment.kt
@@ -505,10 +505,40 @@ class ScanObstacleFragment :
                     box.centerY(), pen
                 )
 
+                var location = ""
+                val w = bitmap.width / 3
+                val h = bitmap.height / 3
+
+                if(centerX in 0.. w){
+                    if(centerY in 0.. h){
+                        location = "11시 방향"
+                    }else if(centerY in h .. h * 2){
+                        location = "9시 방향"
+                    }else if(centerY in h * 2 .. bitmap.height){
+                        location = "7시 방향"
+                    }
+                }else if(centerX in w .. w * 2){
+                    if(centerY in 0.. h){
+                        location = "12시 방향"
+                    }else if(centerY in h .. h * 2){
+                        location = "중앙"
+                    }else if(centerY in h * 2 .. bitmap.height){
+                        location = "6시 방향"
+                    }
+                }else if(centerX in w * 2 .. bitmap.width){
+                    if(centerY in 0.. h){
+                        location = "1시 방향"
+                    }else if(centerY in h .. h * 2){
+                        location = "3시 방향"
+                    }else if(centerY in h * 2 .. bitmap.height){
+                        location = "5시 방향"
+                    }
+                }
+
                 // 음성 출력
                 if (System.currentTimeMillis() - lastSpeakTime > INTERVAL) {
                     lastSpeakTime = System.currentTimeMillis()
-                    speakOut("전방 ${convertedDistance}에 ${it.text}가 있습니다")
+                    speakOut("$location ${convertedDistance}에 ${it.text}가 있습니다")
                 }
             }
         }
