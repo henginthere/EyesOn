5b593f857eef30406c75691d5edf5b34e24699b2
DESKTOP-KVCQHCD\multicampus <aud0725_123@naver.com>
2022-11-14 10:31:28 +0900

    [S07P31D201-144] UPDATE : 엔젤과 연결 Back버튼

--- AOS/EyesOn/presentation/src/main/java/com/d201/eyeson/view/blind/help/BlindHelpActivity.kt
+++ AOS/EyesOn/presentation/src/main/java/com/d201/eyeson/view/blind/help/BlindHelpActivity.kt
@@ -1,10 +1,16 @@
 package com.d201.eyeson.view.blind.help
 
+import android.graphics.RectF
+import android.widget.ImageButton
+import androidx.core.content.ContentProviderCompat.requireContext
 import androidx.navigation.NavController
+import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.fragment.findNavController
 import com.d201.eyeson.R
 import com.d201.eyeson.base.BaseActivity
 import com.d201.eyeson.databinding.ActivityAngelHelpBinding
+import com.d201.eyeson.util.accessibilityEvent
 import dagger.hilt.android.AndroidEntryPoint
 
 private const val TAG = "BlindHelpActivity"
@@ -13,13 +19,27 @@ class BlindHelpActivity : BaseActivity<ActivityAngelHelpBinding>(R.layout.activi
 
     private lateinit var navHostFragment: NavHostFragment
     private lateinit var navController: NavController
+    private lateinit var btnBack:ImageButton
 
     override fun init() {
         initView()
+        initListener()
     }
 
     private fun initView(){
         navHostFragment = supportFragmentManager.findFragmentById(R.id.frame_blind_help) as NavHostFragment
         navController = navHostFragment.navController
+
+    }
+    private fun initListener(){
+        btnBack = findViewById(R.id.btn_back)
+        btnBack.setOnClickListener {
+            onBackPressed()
+        }
+    }
+
+    override fun onBackPressed() {
+        super.onBackPressed()
+        finish()
     }
 }
