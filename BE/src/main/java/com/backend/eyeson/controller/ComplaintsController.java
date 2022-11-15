62b5b7c19a8187a6ad0b72c4a4eb8ae738d16f91
DESKTOP-KVCQHCD\multicampus <sa01070@naver.com>
2022-11-15 13:31:23 +0900

    [[S07P31D201-115] FIX: 주소 null 삭제

--- BE/src/main/java/com/backend/eyeson/controller/ComplaintsController.java
+++ BE/src/main/java/com/backend/eyeson/controller/ComplaintsController.java
@@ -23,6 +23,7 @@ import org.springframework.web.bind.annotation.*;
 import com.google.api.client.json.JsonFactory;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
 import java.nio.file.Path;
 import java.time.LocalDateTime;
 
@@ -109,4 +110,12 @@ public class ComplaintsController {
 
         return new ResponseEntity<>(ResponseFrame.of(result, "민원 처리 완료"), HttpStatus.OK);
     }
+
+//    @ApiParam(value = "TEST API")
+//    @GetMapping(value= "/addressTest")
+//    public String test(@RequestParam String address) throws IOException {
+//        String result = ReverseGeocoding.getAddress(address);
+//        return result;
+//    }
+
 }
